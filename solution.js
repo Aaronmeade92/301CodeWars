f86df5c099c5f32053456b995ac146d5c98e8842
Aaron <aaronmeade9292@gmail.com>
2018-03-15 19:33:25 -0700

    added solution to Abbreviate name

---
+++ solution.js
@@ -0,0 +1,9 @@
+//ABBREVIATE NAME
+
+function abbrevName(name){
+  var title = name.split(' ');
+
+  return title[0].charAt(0).toUpperCase() + '.' + title[1].charAt(0).toUpperCase();
+    // code away
+
+}
