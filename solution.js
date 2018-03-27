3ce71763190cd8a85b5d0add06729ea0c9759ed8
Aaron <aaronmeade9292@gmail.com>
2018-03-26 17:11:11 -0700

    added the solution

--- solution.js
+++ solution.js
@@ -7,3 +7,15 @@ function abbrevName(name){
     // code away
 
 }
+//Insert dashes, had to look up the solutions for this one. Still working on understanding RegEx
+
+
+function insertDash(num) {
+   return num.toString().replace(/[13579](?=[13579])/g, "$&-");
+}
+
+// RegEx validation
+
+function validateUsr(username) {
+  return /^[0-9a-z_]{4,16}$/.test(username)
+}
