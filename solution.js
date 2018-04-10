a96554bd584fce639fab4d5c61f0048d27b9bb0c
Aaron <aaronmeade9292@gmail.com>
2018-04-09 20:47:48 -0700

    this one was easy!

--- solution.js
+++ solution.js
@@ -50,3 +50,28 @@ return str.replace(/[aeiou]/gi, '');
 
 }
 
+// Format words
+
+function formatWords(words){
+  if (!words) return '';
+
+  words = words.filter((word) => {
+    return word.length;
+  });
+
+  if (words.length === 0) {
+    return '';
+  }
+  else if (words.length == 1) {
+    return words[0];
+  }
+  else {
+    return [words.slice(0, words.length -1).join(', '), words.slice(words.length - 1)[0]].join(' and ');
+  }
+}
+
+// Reversed toString
+
+function solution(str){
+  return str.split('').reverse().join('');
+}
